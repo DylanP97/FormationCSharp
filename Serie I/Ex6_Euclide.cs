54eb5805ab70a7781b84f9ec8eb200164f623df4
dylanP97 <dylan.pinheiro.1997@gmail.com>
2024-01-02 15:06:09 +0100

    exercice 6

--- Serie I/Ex6_Euclide.cs
+++ Serie I/Ex6_Euclide.cs
@@ -10,14 +10,23 @@ namespace Serie_I
     {
         public static int Pgcd(int a, int b)
         {
-            //TODO
-            return -1;
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
         }
 
         public static int PgcdRecursive(int a, int b)
         {
-            //TODO
-            return -1;
+            // Cas de base : si b est égal à 0, le PGCD est a
+            if (b == 0)
+                return a;
+
+            // Appel récursif avec les arguments inversés
+            return PgcdRecursive(b, a % b);
         }
     }
 }
